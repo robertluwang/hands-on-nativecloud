5384b1b43aa10b2380eb97cc8e92396a329246b5
Robert Wang <robert.lu.wang@gmail.com>
2021-10-30 10:08:58 -0400

    Create docker-install.sh

---
+++ k8s/docker-install.sh
@@ -0,0 +1,37 @@
+# docker-install.sh
+# handy script to install docker on ubuntu 
+# By Robert Wang @github.com/robertluwang
+# Oct 30, 2021
+
+echo === $(date) Provisioning - docker-install.sh by $(whoami) start
+
+sudo apt-get update -y
+sudo apt-get install -y ca-certificates curl gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+# turn off swap
+sudo swapoff -a
+sudo sed -i '/swap/d' /etc/fstab
+
+sudo mkdir /etc/docker
+cat <<EOF | sudo tee /etc/docker/daemon.json
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+sudo systemctl enable docker
+sudo systemctl daemon-reload
+sudo systemctl restart docker
+
+echo === $(date) Provisioning - docker-install.sh by $(whoami) end
